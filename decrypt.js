ff6c5b9d32fb79ba25105562307a87a7c83aaffc
Steven DeLoach <sfdeloach@gmail.com>
2019-02-07 13:57:11 -0500

    working version

--- decrypt.js
+++ decrypt.js
@@ -1,43 +1,15 @@
-const fs = require('fs');
-const path = require('path');
+var encryptor = require('file-encryptor');
 
-if (!process.argv[2] || !process.argv[3]) {
-  console.log('Usage: node decrypt.js [ENCRYPTED FILE] [OUTPUT FILE]...');
-  process.exit();
+var input_file = process.argv[2];
+var output_file = process.argv[3];
+var key = process.argv[4];
+
+if (!input_file || !output_file || !key) {
+  console.log('Usage: node decrypt.js [INPUT_FILE] [OUTPUT_FILE] [KEY]');
+  process.exit(1);
 }
 
-const outfile = path.basename(process.argv[3]);
-
-fs.readFile(process.argv[2], 'utf8', (err, contents) => {
-  if (err) {
-    console.error(err.message);
-    process.exit(1);
-  }
-
-  let charCodes = [],
-    mult = 0,
-    buffer = '',
-    decryptedResult = '';
-
-  contents.split('').forEach(c => {
-    if (Number.isInteger(+c)) {
-      buffer += c;
-    } else if (buffer != '') {
-      mult = mult > 8 ? 1 : mult + 1;
-      charCodes.push((+buffer + 28 * mult) / mult);
-      buffer = '';
-    }
-  });
-
-  charCodes.forEach(c => {
-    decryptedResult += String.fromCharCode(c);
-  });
-
-  fs.writeFile(outfile, decryptedResult, err => {
-    if (err) {
-      console.error(err.message);
-      process.exit(1);
-    }
-    console.log(outfile + ' created.');
-  });
+encryptor.decryptFile(input_file, output_file, key, function(err) {
+  if (err) console.error(err.message);
+  else console.log(output_file + ' has been created');
 });
--- encrypt.js
+++ encrypt.js
@@ -1,62 +1,15 @@
-const fs = require('fs');
-const path = require('path');
+var encryptor = require('file-encryptor');
 
-// generate a random sequence of non-digit characters
-function dash(min, max) {
-  const length = rand(min, max);
-  let result = '';
-  let charCode = 0;
-  for (let i = 0; i < length; i++) {
-    while (
-      (0 <= charCode && charCode < 32) ||
-      (47 < charCode && charCode < 58) ||
-      126 < charCode
-    ) {
-      charCode = rand(30, 128);
-    }
-    result += String.fromCharCode(charCode);
-    charCode = 0;
-  }
-  return result;
+var input_file = process.argv[2];
+var output_file = process.argv[3];
+var key = process.argv[4];
+
+if (!input_file || !output_file || !key) {
+  console.log('Usage: node encrypt.js [INPUT_FILE] [OUTPUT_FILE] [KEY]');
+  process.exit(1);
 }
 
-// generate a random whole number between min and max
-function rand(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) + min);
-}
-
-if (!process.argv[2]) {
-  console.log('Usage: node encrypt.js [FILE]...');
-  process.exit();
-}
-
-const outfile = path.basename(process.argv[2] + '.enc');
-
-fs.readFile(process.argv[2], 'utf8', (err, contents) => {
-  if (err) {
-    console.error(err.message);
-    process.exit(1);
-  }
-
-  let charCodes = [],
-    mult = 0,
-    encryptedResult = '';
-
-  contents.split('').forEach(c => {
-    charCodes.push(c.charCodeAt());
-  });
-
-  charCodes.map(e => {
-    mult = mult > 8 ? 1 : mult + 1;
-    encryptedResult += e * mult - 28 * mult + dash(3, 4);
-    return;
-  });
-
-  fs.writeFile(outfile, encryptedResult, err => {
-    if (err) {
-      console.error(err.message);
-      process.exit(1);
-    }
-    console.log(outfile + ' created.');
-  });
+encryptor.encryptFile(input_file, output_file, key, function(err) {
+  if (err) console.error(err.message);
+  else console.log(output_file + ' has been created');
 });
